f4d4558205ab2fe577e2731f7137f9e04bcf93ca
Chris Bentzel <cbentzel@gmail.com>
2024-12-26 21:01:55 -0500

    Fixes for vite

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// filepath: /Users/cbentzel/code/rlrental/vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+});
